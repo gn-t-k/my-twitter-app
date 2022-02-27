fb9b4a4efd6d54ce31c508512a05aac6e8b764dc
gn-t-k <gntk1995422@gmail.com>
2022-02-27 16:24:21 +0900

    環境構築

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// import * as clc from "cli-color";
+// import * as commandLineArgs from "command-line-args";
+// import * as commandLineUsage from "command-line-usage";
+import "source-map-support/register";
+
+async function main() {
+  console.log("hello");
+}
+
+main();
