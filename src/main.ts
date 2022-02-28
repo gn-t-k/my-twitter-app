8771531b1fa92326a72667e574d3ee055e27e502
gn-t-k <gntk1995422@gmail.com>
2022-02-28 09:36:30 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
-
-// import * as clc from "cli-color";
-// import * as commandLineArgs from "command-line-args";
-// import * as commandLineUsage from "command-line-usage";
 import "source-map-support/register";
 
-async function main() {
-  console.log("hello");
-  console.log(process.argv);
-}
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const main = () => {
+  // eslint-disable-next-line no-unused-expressions
+  yargs(hideBin(process.argv))
+    .commandDir("commands")
+    .strict()
+    .alias({ h: "help" }).argv;
+};
 
 main();
