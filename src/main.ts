ffe0e8e7b336d4a6a59d64b1b2a0b539361355e7
gn-t-k <gntk1995422@gmail.com>
2022-03-06 15:09:35 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 #!/usr/bin/env node
 import "source-map-support/register";
+import "dotenv/config.js";
 
-const main = () => {
-  const args = process.argv.slice(2);
+import Twitter from "twitter-api-v2";
 
-  console.log(args);
+const main = async () => {
+  // const args = process.argv.slice(2);
+
+  const appKey = process.env.API_KEY ?? "";
+  const appSecret = process.env.API_KEY_SECRET ?? "";
+  const accessToken = process.env.ACCESS_TOKEN ?? "";
+  const accessSecret = process.env.ACCESS_TOKEN_SECRET ?? "";
+
+  const client = new Twitter({
+    appKey,
+    appSecret,
+    accessToken,
+    accessSecret,
+  });
+
+  const foundUsers = await client.v1.searchUsers("test");
+
+  console.log({ foundUsers });
 };
 
 main();
