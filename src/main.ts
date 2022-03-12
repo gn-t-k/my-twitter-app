6a225c1c7db4c0598488dac262459e55de3d7562
gn-t-k <gntk1995422@gmail.com>
2022-03-12 21:30:35 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,6 @@ const main = async () => {
   });
   const readOnlyClient = client.readOnly;
 
-  let authorIDList: string[] = [];
-
   const tweets = await readOnlyClient.v2.search("JavaScript", {
     expansions: ["author_id"],
     max_results: 100,
@@ -29,11 +27,9 @@ const main = async () => {
   if (tweets.data.meta.result_count === 0) {
     console.log("no user found");
   } else {
-    authorIDList = authorIDList.concat(
-      tweets.data.data
-        .map((data) => data.author_id)
-        .flatMap((data) => (data === undefined ? [] : [data]))
-    );
+    const authorIDList = tweets.data.data
+      .map((data) => data.author_id)
+      .flatMap((data) => (data === undefined ? [] : [data]));
     const users = await client.v2.users(authorIDList);
 
     console.log(users.data.map((data) => data.username));
