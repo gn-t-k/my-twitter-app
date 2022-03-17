ea177c14c2126f3dff18d2ccc43f3dea6cd80d64
gn-t-k <gntk1995422@gmail.com>
2022-03-17 09:03:14 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,21 @@ import "dotenv/config.js";
 
 import Twitter from "twitter-api-v2";
 
-const main = async () => {
-  // const args = process.argv.slice(2);
+const appKey = process.env.API_KEY ?? "";
+const appSecret = process.env.API_KEY_SECRET ?? "";
+const accessToken = process.env.ACCESS_TOKEN ?? "";
+const accessSecret = process.env.ACCESS_TOKEN_SECRET ?? "";
 
-  const appKey = process.env.API_KEY ?? "";
-  const appSecret = process.env.API_KEY_SECRET ?? "";
-  const accessToken = process.env.ACCESS_TOKEN ?? "";
-  const accessSecret = process.env.ACCESS_TOKEN_SECRET ?? "";
+const client = new Twitter({
+  appKey,
+  appSecret,
+  accessToken,
+  accessSecret,
+});
+const readOnlyClient = client.readOnly;
 
-  const client = new Twitter({
-    appKey,
-    appSecret,
-    accessToken,
-    accessSecret,
-  });
-  const readOnlyClient = client.readOnly;
-
-  const tweets = await readOnlyClient.v2.search("JavaScript", {
+const seachUserByWord = async (word: string) => {
+  const tweets = await readOnlyClient.v2.search(word, {
     expansions: ["author_id"],
     max_results: 100,
   });
@@ -33,6 +31,16 @@ const main = async () => {
     const users = await client.v2.users(authorIDList);
 
     console.log(users.data.map((data) => data.username));
+  }
+};
+
+const main = async () => {
+  const args = process.argv.slice(2);
+
+  if (args[0] === "user") {
+    await seachUserByWord(args[1]);
+  } else {
+    console.log("no command");
   }
 };
 
